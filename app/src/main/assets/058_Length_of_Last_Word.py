feac40c3f3c616b9d25d06f585e1cb7fac91bac5
jakehoare <jake_hoare@hotmail.com>
2019-11-03 10:03:31 +1100

    Update assets with new solutions, index and help text.

--- app/src/main/assets/058_Length_of_Last_Word.py
+++ app/src/main/assets/058_Length_of_Last_Word.py
@@ -2,11 +2,14 @@ _author_ = 'jake'
 _project_ = 'leetcode'
 
 # https://leetcode.com/problems/length-of-last-word/
-# Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+# return the length of last word in the string.
 # If the last word does not exist, return 0.
-# Note: A word is defined as a character sequence consists of non-space characters only
+# Note: A word is defined as a character sequence consists of non-space characters only.
 
-# Start at last character and decrement i.  Set variable end when non-blank is seen.  Return at next non-blank.
+# Start at last character and decrement i.
+# Set variable end when non-blank is seen.
+# Return at next non-blank.
 # Time - O(n)
 # Space - O(1)
 
@@ -16,7 +19,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        i = len(s)-1
+        i = len(s) - 1
         end = -1
 
         while i >= 0:
